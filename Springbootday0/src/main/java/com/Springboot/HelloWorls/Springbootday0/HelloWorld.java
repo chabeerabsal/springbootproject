50887540dcf39c63798ac69ed3d76b52d97f77ba
chabeer absal <chabeer002@gmail.com>
2025-04-24 23:09:39 +0530

    add

---
+++ Springbootday0/src/main/java/com/Springboot/HelloWorls/Springbootday0/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.Springboot.HelloWorls.Springbootday0;
+
+public class HelloWorld extends Training{
+    public void helloWorld() {
+        System.out.println("Hello World");
+
+    }
+}
